934b5c664ec913854b3557356da507cce762fc11
runner <runner@zendesk.com>
2025-09-11 09:27:45 +0000

    [RELEASE] New version 6.2.0

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
                  .exact("6.0.2")),
         .package(name: "ZendeskCommonUISDK",
                  url: "https://github.com/zendesk/commonui_sdk_ios",
-                 .exact("9.1.0"))
+                 .exact("9.2.0"))
     ],
     targets: [
         .binaryTarget(
